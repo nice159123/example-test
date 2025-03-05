b42d957ca9699d6c3d40a85650ef3509fa0b89e4
Nice <62437778+nice159123@users.noreply.github.com>
2025-03-05 17:00:23 +0700

    Update main.go

--- Example1/main.go
+++ Example1/main.go
@@ -13,18 +13,25 @@ func processArrays(arr1, arr2 []string) ([]string, []string) {
 	for i := 0; i < len(arr1); i++ {
 		countMap[arr1[i]]++
 	}
-	
+
 	for i := 0; i < len(arr2); i++ {
 		countMap[arr2[i]]++
 	}
 
-	keys := make([]string, 0, len(countMap))
-	for key := range countMap {
-		keys = append(keys, key)
+	keys := make([]string, len(countMap))
+	index := 0
+	mapKeys := make([]string, 0, len(countMap))
+	for k := range countMap {
+		mapKeys = append(mapKeys, k)
+	}
+
+	for i := 0; i < len(mapKeys); i++ {
+		keys[index] = mapKeys[i]
+		index++
 	}
 
 	sort.Strings(keys)
-	
+
 	for i := 0; i < len(keys); i++ {
 		key := keys[i]
 		if countMap[key] == 1 {
@@ -42,8 +49,8 @@ func main() {
 
 	union, diff := processArrays(arr1, arr2)
 
-	fmt.Println("Input Arr 1", arr1)
-	fmt.Println("Input Arr 2", arr2)
+	fmt.Println("Input Arr 1:", arr1)
+	fmt.Println("Input Arr 2:", arr2)
 	fmt.Println("Output Union:", union)
 	fmt.Println("Output Difference:", diff)
 }
