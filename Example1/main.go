74dfe8a7e049bd8d35b2acf7e5d24efcd09c2923
devdog <devdog.n@gmail.com>
2025-03-05 16:42:57 +0700

    first commit

--- Example1/main.go
+++ Example1/main.go
@@ -2,52 +2,36 @@ package main
 
 import "fmt"
 
-func uniqueArray(arr1, arr2 []string) []string {
-	uniqueMap := make(map[string]bool)
-	var result []string
-	for _, val := range arr1 {
-		uniqueMap[val] = true
-	}
-	for _, val := range arr2 {
-		uniqueMap[val] = true
-	}
-	for key := range uniqueMap {
-		result = append(result, key)
-	}
-	return result
-}
-
-func uniqueArrayDifference(arr1, arr2 []string) []string {
+func processArrays(arr1, arr2 []string) ([]string, []string) {
 	countMap := make(map[string]int)
-	var result []string
+	var uniqueResult []string
+	var diffResult []string
+
 	for _, val := range arr1 {
 		countMap[val]++
 	}
 	for _, val := range arr2 {
 		countMap[val]++
 	}
+
 	for key, count := range countMap {
 		if count == 1 {
-			result = append(result, key)
+			diffResult = append(diffResult, key)
 		}
+		uniqueResult = append(uniqueResult, key)
 	}
-	return result
-}
 
-func arrayProcess(arr1, arr2 []string) (result1 []string, result2 []string) {
-	result1 = uniqueArray(arr1, arr2)
-	result2 = uniqueArrayDifference(arr1, arr2)
-	return result1, result2
+	return uniqueResult, diffResult
 }
 
 func main() {
 	arr1 := []string{"a", "b", "c"}
 	arr2 := []string{"b", "c", "d"}
 
-	unionResult, diffResult := arrayProcess(arr1, arr2)
+	union, diff := processArrays(arr1, arr2)
 
 	fmt.Println("Input Arr 1", arr1)
 	fmt.Println("Input Arr 2", arr2)
-	fmt.Println("Output Union:", unionResult)
-	fmt.Println("Output Difference:", diffResult)
+	fmt.Println("Output Union:", union)
+	fmt.Println("Output Difference:", diff)
 }
--- Example2/README.md
+++ Example2/README.md
@@ -1,7 +1,8 @@
 > ออกแบบ API ที่สามารถรองรับ 100,000 requests per second (RPS) ได้โดยไม่ให้เซิร์ฟเวอร์ล่ม และคำนึงถึงค่า server ด้วยเป็นหลัก โดยใช้ ใช้ techstack อะไรก็ได้
 --- 1 request ต้อง ตรวจสอบ check user และ ตรวจสอบยอดเงิน จาก api ภายนอก
 
-![image](https://github.com/nice159123/test-after-interview/blob/main/Example2/example2.drawio.png)
+![flow](https://github.com/nice159123/test-after-interview/blob/main/Example2/example2-flow.png)
+![diagram](https://github.com/nice159123/test-after-interview/blob/main/Example2/example2-diagram.png)
 # Tech Stack ที่เลือก
 
 | หัวข้อ        | เทคโนโลยี             | งาน
